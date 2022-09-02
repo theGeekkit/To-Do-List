e5b075501dcc6ba032e5cd3bda4fd249d895bb51
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-09-01 19:48:38 -0500

    9-1-22
    Co-authored-by: chrisHembree <chrisHembree@users.noreply.github.com>
    Co-authored-by: kylesteinmann <kylesteinmann@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-let addTaskButton = document.querySelector(".addTaskButton");
-addTaskButton.addEventListener('click', () => {
-    let todoContainer = document.querySelector(".todoContainer");
-    let todoInput = document.createElement("input");
-    todoInput.type = 'text';
+let todoList = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// let addTaskButton = document.querySelector(".addTaskButton");
+// addTaskButton.addEventListener('click', () => {
+//     let todoContainer = document.querySelector(".todoContainer");
+//     let todoInput = document.createElement("input");
+//     todoInput.type = 'text';
     
     
-    let todoCheckBox = document.createElement("input");
-    todoCheckBox.type = 'checkBox';
+//     let todoCheckBox = document.createElement("input");
+//     todoCheckBox.type = 'checkBox';
     
-    todoContainer.appendChild(todoCheckBox)
-    todoContainer.appendChild(todoInput)
-});
+//     todoContainer.appendChild(todoCheckBox)
+//     todoContainer.appendChild(todoInput)
+// });
 
+let taskButton = document.querySelector("#addTaskButton");
+let task = document.querySelector("#taskInput");
+taskButton.addEventListener("click", () => {
+    
+    let taskValue = task.value; 
+     todoList.push(taskValue);
+     console.log(todoList)
+     localStorage.setItem("tasks", JSON.stringify(todoList))
+     location.reload()
+})
 
-
-
+console.log(todoList)
 
 
 
