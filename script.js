b0a9050119b464a56f6532da485fc964aad6f574
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-09-08 20:17:31 -0500

    Co-authored-by: kylesteinmann <kylesteinmann@users.noreply.github.com> Co-authored-by: chrisHembree <chrisHembree@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,52 +1,65 @@
 let todoList = JSON.parse(localStorage.getItem("tasks")) || [];
 
-// let addTaskButton = document.querySelector(".addTaskButton");
-// addTaskButton.addEventListener('click', () => {
-//     let todoContainer = document.querySelector(".todoContainer");
-//     let todoInput = document.createElement("input");
-//     todoInput.type = 'text';
-    
-    
-//     let todoCheckBox = document.createElement("input");
-//     todoCheckBox.type = 'checkBox';
-    
-//     todoContainer.appendChild(todoCheckBox)
-//     todoContainer.appendChild(todoInput)
-// });
-
+let toDoListContainer = document.querySelector(".toDoListContainer");
 let taskButton = document.querySelector("#addTaskButton");
-let task = document.querySelector("#taskInput");
+let taskInput = document.querySelector("#taskInput");
+let taskValue = taskInput.value;
+
 taskButton.addEventListener("click", () => {
-    
-    let taskValue = task.value; 
-     todoList.push(taskValue);
-     console.log(todoList)
-     localStorage.setItem("tasks", JSON.stringify(todoList))
-     location.reload()
-})
-
-console.log(todoList)
-
-function navigate(url) {
-    location.href = url
-}
-
-function addTask(task) {
-    const tasks = tryToGetTasks()
-    console.log(tasks)
-    tasks.push(task)
-
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-}
-
-function tryToGetTasks() {
-    try {
-        return JSON.parse(localStorage.getItem('tasks')) || []
-    } catch(e) {
-        console.error('Oh no what happened?', e)
-        return []
-    }
-}
+ 
+  todoList.push(taskValue);
+  
+  localStorage.setItem("tasks", JSON.stringify(todoList));
+});
 
 
+  function generateTaskList() {
+  
+    let listAddition = document.createElement("div");
 
+    listAddition.innerText(todoList[0]);
+
+    toDoListContainer.appendChild(listAddition);
+  }
+
+  generateTaskList()
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // console.log(todoList);
+
+// function navigate(url) {
+//   location.href = url;
+// }
+
+// function addTask(task) {
+//   const tasks = tryToGetTasks();
+//   console.log(tasks);
+//   tasks.push(task);
+
+//   localStorage.setItem("tasks", JSON.stringify(tasks));
+// }
+
+// function tryToGetTasks() {
+//   try {
+//     return JSON.parse(localStorage.getItem("tasks")) || [];
+//   } catch (e) {
+//     console.error("Oh no what happened?", e);
+//     return [];
+//   }
+// }
